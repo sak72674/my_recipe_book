057e1a1c1d159cd551aea36fe5dd7193493f6a9d
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-09-20 16:07:21 +0900

    Add Seed date

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "saori", email: "demo@demo", password: "111111")
+
+Genre.create!(name: "肉")
+Genre.create!(name: "魚")
+Genre.create!(name: "野菜")
+Genre.create!(name: "麺")
+Genre.create!(name: "その他")
+Genre.create!(name: "おやつ")
+
