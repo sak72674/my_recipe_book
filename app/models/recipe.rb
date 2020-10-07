b331a5214d59ef2c65f4b318d17902681b5ca719
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-10-08 00:51:20 +0900

    Add users view & ransack

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -18,4 +18,10 @@ class Recipe < ApplicationRecord
 	belongs_to :genre
 	belongs_to :user
 
+	has_many :favorites, dependent: :destroy
+
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
+
 end
