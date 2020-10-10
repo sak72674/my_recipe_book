47a69628e9af476e4e25c1fa21d427317743a9d5
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-10-10 22:02:14 +0900

    Add model test

--- spec/model/bookmark_spec.rb
+++ spec/model/bookmark_spec.rb
@@ -1,1 +1,28 @@
 require 'rails_helper'
+
+RSpec.describe 'Bookmarkモデルテスト', type: :model do
+  let!(:user) { build(:user) }
+  let!(:genre) { build(:genre) }
+  let!(:bookmark) { build(:bookmark) }
+	subject { bookmark. valid? }
+
+ 	it '名前がなければ登録できないこと' do
+ 		bookmark.name = ""
+    expect(bookmark).to_not be_valid
+  end
+
+  it 'ジャンルがなければ登録できないこと' do
+  	bookmark.genre_id = nil
+  	expect(bookmark).to_not be_valid
+	end
+
+	it 'URLがなければ登録できないこと' do
+  	bookmark.url = ""
+  	expect(bookmark).to_not be_valid
+	end
+
+	it 'サイト名がなければ登録できないこと' do
+  	bookmark.site = ""
+  	expect(bookmark).to_not be_valid
+	end
+end
--- spec/model/recipe_spec.rb
+++ spec/model/recipe_spec.rb
@@ -1,1 +1,34 @@
 require 'rails_helper'
+
+RSpec.describe 'Recipeモデルテスト', type: :model do
+  describe 'バリデーションテスト' do
+    let!(:user) { build(:user) }
+    let!(:genre) { build(:genre) }
+    let!(:recipe) { build(:recipe)}
+    context 'nameカラム' do
+     	it '空欄でないこと' do
+        recipe.name = ''
+        recipe.valid?
+     		expect(recipe).to_not be_valid
+     	end
+
+      it 'genre_idカラム' do
+      	recipe.genre_id = nil
+        recipe.valid?
+      	expect(recipe).to_not be_valid
+    	end
+
+    	it 'introductionカラム' do
+      	recipe.introduction = ""
+        recipe.valid?
+      	expect(recipe).to_not be_valid
+    	end
+
+    	it 'commentカラム' do
+      	recipe.comment = ""
+        recipe.valid?
+        expect(recipe).to_not be_valid
+    	end
+    end
+  end
+end
